5f5704d126981e5d37fcbf02bbe19fce3434e6e6
Nicolas <nicolasarena.nico@gmail.com>
2024-02-08 22:42:30 +0100

    siteweb login

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx()],
-  site: 'https://vratskyi.github.io',
+  site: 'https://www.nicolas-arena.dev',
   prefetch: {
     prefetchAll: true,
     defaultStrategy: 'viewport'
